15aa1c4c5878324a1599583086dff48dac3b40b8
Anish K <akneni@gmail.com>
2024-07-05 23:43:40 -0400

    Add functionality to suport storing zero-knowledge encrypted ngrok keys

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,13 @@
-use shuttle_runtime::{Secrets, SecretStore};
-use rocket::{
-    get, 
-    routes, 
-    State, 
-    response::status,
-    http::Status,
+use mongodb::{
+    bson::{doc, Binary, Bson},
+    options::ClientOptions,
+    Client, Collection,
 };
-use mongodb::{Client, options::ClientOptions, Collection, bson::doc};
-use serde::{Serialize, Deserialize};
-use tynkerbase_universal;
+use rocket::{get, http::Status, post, response::status, routes, State};
+use serde::{Deserialize, Serialize};
+use shuttle_runtime::{SecretStore, Secrets};
 use std::time::{SystemTime, UNIX_EPOCH};
+use tynkerbase_universal;
 
 const DB_NAME: &str = "tyb-server-db";
 const USER_AUTH_COL: &str = "user-auth";
@@ -20,15 +18,17 @@ struct UserAuthData {
     pass_sha256: String,
     creation_time: f64,
     salt: String,
+    ngrok_aes: Option<Vec<u8>>,
 }
 
 impl UserAuthData {
     fn new(email: &str, pass_sha256: &str) -> Self {
         let start = SystemTime::now();
-        let t = start.duration_since(UNIX_EPOCH)
+        let t = start
+            .duration_since(UNIX_EPOCH)
             .expect("Time went backwards");
         let t = t.as_secs() as f64 + t.subsec_nanos() as f64 * 1e-9;
-        
+
         let salt = tynkerbase_universal::crypt_utils::gen_salt();
 
         UserAuthData {
@@ -36,46 +36,127 @@ impl UserAuthData {
             pass_sha256: pass_sha256.to_string(),
             creation_time: t,
             salt: salt,
+            ngrok_aes: None,
         }
     }
 }
 
-#[get("/login?<email>&<pass_sha256>")]
-async fn login (email: &str, pass_sha256: &str, client: &State<Client>) -> status::Custom<String> {
+async fn authenitcate_req(
+    email: &str,
+    pass_sha256: &str,
+    client: &State<Client>,
+) -> Result<UserAuthData, status::Custom<&'static str>> {
     let collection: Collection<UserAuthData> = client.database(DB_NAME).collection(USER_AUTH_COL);
-    let res = collection.find_one(doc!{"email": email})
-        .await;
+    let res = collection.find_one(doc! {"email": email}).await;
 
-    match res {
-        Ok(Some(user_auth_data)) => {
-            if user_auth_data.pass_sha256 != pass_sha256  {
-                return status::Custom(Status::Unauthorized, "Incorrect password".to_string())
-            }
-
-            return status::Custom(Status::Ok, user_auth_data.salt);
-        },
-        Ok(None) => return status::Custom(Status::BadRequest, "User not found".to_string()),
-        Err(e) => return status::Custom(Status::InternalServerError, format!("Database query failed: {}", e)),
+    let res = match res {
+        Ok(r) => r,
+        Err(_) => {
+            return Err(status::Custom(
+                Status::InternalServerError,
+                "Failed to access database",
+            ))
+        }
     };
+
+    let res = match res {
+        Some(r) => r,
+        None => {
+            return Err(status::Custom(
+                Status::BadRequest,
+                "No account with specified email exists",
+            ))
+        }
+    };
+
+    if res.pass_sha256 != pass_sha256 {
+        return Err(status::Custom(Status::Forbidden, "inocrrect password"));
+    }
+
+    Ok(res)
+}
+
+#[get("/login?<email>&<pass_sha256>")]
+async fn login(email: &str, pass_sha256: &str, client: &State<Client>) -> status::Custom<String> {
+    let res = match authenitcate_req(email, pass_sha256, client).await {
+        Ok(r) => r,
+        Err(e) => return status::Custom(e.0, e.1.to_string()),
+    };
+
+    status::Custom(Status::Ok, res.salt.to_string())
 }
 
 #[get("/create-account?<email>&<pass_sha256>")]
-async fn create_account (email: &str, pass_sha256: &str, client: &State<Client>) -> status::Custom<&'static str> {
+async fn create_account(
+    email: &str,
+    pass_sha256: &str,
+    client: &State<Client>,
+) -> status::Custom<&'static str> {
     let collection: Collection<UserAuthData> = client.database(DB_NAME).collection(USER_AUTH_COL);
-    let res = collection.find_one(doc!{"email": email})
-        .await;
+    let res = collection.find_one(doc! {"email": email}).await;
 
     match res {
         Ok(None) => {
             let new_user = UserAuthData::new(email, pass_sha256);
             let ins_res = collection.insert_one(new_user).await;
             if let Err(_e) = ins_res {
-                return status::Custom(Status::InternalServerError, "unable to insert data into database");
+                return status::Custom(
+                    Status::InternalServerError,
+                    "unable to insert data into database",
+                );
             }
             status::Custom(Status::Ok, "account created successfully")
-        },
+        }
         Ok(Some(_)) => status::Custom(Status::BadRequest, "user already exists"),
         Err(_) => status::Custom(Status::InternalServerError, "Database query failed"),
+    }
+}
+
+#[post("/save-ng-auth?<email>&<pass_sha256>", data = "<data>")]
+async fn save_ng_auth(
+    email: &str,
+    pass_sha256: &str,
+    data: Vec<u8>,
+    client: &State<Client>,
+) -> status::Custom<&'static str> {
+    let _ = match authenitcate_req(email, pass_sha256, client).await {
+        Ok(r) => r,
+        Err(e) => return e,
+    };
+
+    let collection: Collection<UserAuthData> = client.database(DB_NAME).collection(USER_AUTH_COL);
+
+    let update = Bson::Binary(Binary {
+        subtype: mongodb::bson::spec::BinarySubtype::Generic,
+        bytes: data,
+    });
+    let status = collection
+        .update_one(doc! {"email": email}, doc! {"$set": {"ngrok_aes": update}})
+        .await;
+    if let Err(_) = status {
+        return status::Custom(Status::BadRequest, "Error updating database");
+    }
+
+    status::Custom(Status::Ok, "success")
+}
+
+#[get("/get-ng-auth?<email>&<pass_sha256>")]
+async fn get_ng_auth(
+    email: &str,
+    pass_sha256: &str,
+    client: &State<Client>,
+) -> status::Custom<Vec<u8>> {
+    let res = match authenitcate_req(email, pass_sha256, client).await {
+        Ok(r) => r,
+        Err(e) => return status::Custom(e.0, e.1.as_bytes().to_vec()),
+    };
+
+    match res.ngrok_aes {
+        Some(r) => status::Custom(Status::Ok, r),
+        None => status::Custom(
+            Status::NotFound,
+            "ngrok key doesn't exist".to_string().into_bytes(),
+        ),
     }
 }
 
@@ -86,7 +167,9 @@ fn index() -> &'static str {
 
 #[shuttle_runtime::main]
 async fn main(#[Secrets] secret_store: SecretStore) -> shuttle_rocket::ShuttleRocket {
-    let mongo_auth_uri = secret_store.get("MONGO_AUTH_URI").expect("Secret `MONGO_AUTH_URI` does not exist.");
+    let mongo_auth_uri = secret_store
+        .get("MONGO_AUTH_URI")
+        .expect("Secret `MONGO_AUTH_URI` does not exist.");
     let client = ClientOptions::parse(&mongo_auth_uri)
         .await
         .expect("Unable to connect to mongo database");
@@ -96,6 +179,7 @@ async fn main(#[Secrets] secret_store: SecretStore) -> shuttle_rocket::ShuttleRo
     let rocket = rocket::build()
         .mount("/", routes![index])
         .mount("/auth", routes![login, create_account])
+        .mount("/ngrok", routes![save_ng_auth, get_ng_auth])
         .manage(client);
 
     Ok(rocket.into())
